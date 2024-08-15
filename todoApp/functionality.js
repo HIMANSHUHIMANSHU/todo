0b3b0dd9c9d8018497fc6bd075382b5ea9cd96ea
Himanshu Sharma <himanshuroxx27@gmail.com>
2024-08-15 19:25:52 +0530

    changes are done

---
+++ todoApp/functionality.js
@@ -0,0 +1,50 @@
+const input= document.querySelector('input');
+const button = document.querySelector("button");
+const taskList= document.querySelector(".list");
+
+
+const inputhandler= function ()
+{
+    const newTask= input.value;
+    const newElem=createTask(newTask);
+        // addtotask.innerText = taskname;
+    taskList.appendChild(newElem);
+    input.value="";
+    const deleteBtn= newElem.children[1];
+    deleteBtn.addEventListener('click', function () {
+
+        newElem.remove();
+            
+    })
+    
+}
+
+function createTask(newTask)
+{
+    const div= document.createElement("div");
+    const li= document.createElement('li');
+    const button= document.createElement('button');
+    button.innerText="Delete";
+
+    li.textContent=newTask;
+    div.appendChild(li);
+    div.appendChild(button);
+    return div;
+
+} 
+button.addEventListener("click",inputhandler);   
+    
+    
+    
+    
+// const button1= document.createElement('button');
+// button1.innerText= "Delete";
+// button1.addEventListener("click",function ()
+//  {
+//     addtotask.remove;
+        
+//  })
+// div.appendChild(button1);
+
+
+ 
